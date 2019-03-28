043eea0f90ec442b3b24d4149effad3524e45e80
Somyaranjan Kar <somyaranjan.kar@qleverlabs.in>
2019-03-28 13:14:16 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,218 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-let fs = require('fs'),
-PDFParser = require("pdf2json");
+let fs = require("fs"),
+  PDFParser = require("pdf2json");
 
-var validator = require('validator');
+var validator = require("validator");
 
-
-var jsonXlsx = require('icg-json-to-xlsx');
-var path     = require('path');
-
-
+var jsonXlsx = require("icg-json-to-xlsx");
+var path = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
-
 
 router.get("/show-the-json", (req, res) => {
-
-
-
-let pdfParser = new PDFParser();
-
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-pdfParser.on("pdfParser_dataReady", pdfData => {
-  //fs.writeFile("./pdf2json/test/F1040EZ.json", JSON.stringify(pdfData));
-  //var theData = JSON.stringify(pdfData);
-
-  //var theJsonData = JSON.parse(theData);
-  var tournament = [];
-  pdfData.formImage.Pages.forEach((thePageObject, index) => {
-    var tournamentName;
-    if( index > 0) {
-
-      
-      var theTournamentName = "";
-      var number = "";
-      var theSchoolName = "";
-      var teamName = "";
-      var GPA = "";
-      var SAT = "";
-      var ACT = "";
-      var cell = "";
-      var contact = "";
-      var DOB = "";
-      var email = "";
-
-      var theObj = {};
-
-      var theTournamentArray = [];
-      
-      thePageObject.Texts.forEach(( theText ) => {
-
-        if (theText.x == 2 && theText.y == 2.038) {
-
-          if (isNaN(decodeURIComponent(theText.R[0].T))) {
-            theTournamentName = decodeURIComponent(theText.R[0].T);
-          }
-
-        }
-
-
-        if (theText.x == 2) {
-          
-          if (!isNaN(decodeURIComponent(theText.R[0].T))) {
-            number = decodeURIComponent(theText.R[0].T);
-          }
-                    
-          }
-
-
-        
-
-        if(theText.x == 3.563 ) {
-          var texta = decodeURIComponent(theText.R[0].T);
-
-            if(texta.includes('@') && texta.includes('.')) {
-              email =texta;
-              theObj.email = email;
-
-              theTournamentArray.push(theObj);
-              
-                          theTournamentName = "";
-                          number = "";
-                          theSchoolName = "";
-                          teamName = "";
-                          GPA = "";
-                          SAT = "";
-                          ACT = "";
-                          cell = "";
-                          contact = "";
-                          DOB = "";
-                          email = "";
-
-
-            }
-            
-
-
-           
-
-          
-        }
-
-
-        if(theText.x == 3.563) {
-          var textb = decodeURIComponent(theText.R[0].T);
-
-          if(!validator.isEmail(textb)) {
-            teamName = textb;
-          }
-            
-          
-        }
-
-        if(theText.x == 23.875) {
-          var text2 = decodeURIComponent(theText.R[0].T);
-          if(text2 != "GPA") {
-            theSchoolName = decodeURIComponent(theText.R[0].T);
-          }
-
-
-        }
-
-        if(theText.x == 25.125) {
-          var text = parseInt(decodeURIComponent(theText.R[0].T));
-          if(text != NaN) {
-            GPA = decodeURIComponent(theText.R[0].T);
-          }
-        }
-
-        if(theText.x == 27.625) {
-          SAT = decodeURIComponent(theText.R[0].T);
-        }
-
-        if(theText.x == 30.125) {
-          ACT = decodeURIComponent(theText.R[0].T);
-        }
-
-        if(theText.x == 13.563) {
-          cell = decodeURIComponent(theText.R[0].T);
-        }
-
-        if(theText.x == 19.5) {
-          contact = decodeURIComponent(theText.R[0].T);
-        }
-
-        if(theText.x == 32.313) {
-          DOB = decodeURIComponent(theText.R[0].T);
-        }
-
-
-
-        theObj = {
-          theTournamentName,
-          number,
-          teamName,
-          email,
-          cell,
-          contact,
-          theSchoolName,
-          GPA,
-          SAT,
-          ACT,
-          DOB
-
-
-        }
-        
-
-      });
-
-      tournament.push(theTournamentArray);
-
-
-
-    }
-
-
-  })
-
-
-
-
-  //writeToExcel(tournament)
-  res.send(pdfData);
-
-
-});
-
-
-pdfParser.loadPDF("./files/2017_T3_Fall_Face_Off.pdf");
-})
-
-
-router.get("/show-the-json-two", (req, res) => {
-  
-  
-  
   let pdfParser = new PDFParser();
-  
-  pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+
+  pdfParser.on("pdfParser_dataError", errData =>
+    console.error(errData.parserError)
+  );
   pdfParser.on("pdfParser_dataReady", pdfData => {
     //fs.writeFile("./pdf2json/test/F1040EZ.json", JSON.stringify(pdfData));
     //var theData = JSON.stringify(pdfData);
-  
+
     //var theJsonData = JSON.parse(theData);
     var tournament = [];
     pdfData.formImage.Pages.forEach((thePageObject, index) => {
       var tournamentName;
-      if( index > 0) {
-  
-        
+      if (index > 0) {
         var theTournamentName = "";
         var number = "";
         var theSchoolName = "";
@@ -224,116 +39,89 @@ router.get("/show-the-json-two", (req, res) => {
         var contact = "";
         var DOB = "";
         var email = "";
-  
+
         var theObj = {};
-  
+
         var theTournamentArray = [];
-        
-        thePageObject.Texts.forEach(( theText ) => {
-  
+
+        thePageObject.Texts.forEach(theText => {
           if (theText.x == 2 && theText.y == 2.038) {
-  
             if (isNaN(decodeURIComponent(theText.R[0].T))) {
               theTournamentName = decodeURIComponent(theText.R[0].T);
             }
-  
           }
-  
-  
+
           if (theText.x == 2) {
-            
             if (!isNaN(decodeURIComponent(theText.R[0].T))) {
               number = decodeURIComponent(theText.R[0].T);
             }
-                      
-            }
-  
-  
-          
-  
-          if(theText.x == 3.563 ) {
-            var texta = decodeURIComponent(theText.R[0].T);
-  
-              if(texta.includes('@') && texta.includes('.')) {
-                email =texta;
-                theObj.email = email;
-  
-                
-  
-  
-              }
-              
-  
-  
-             
-  
-            
           }
-  
-  
-          if(theText.x == 3.563) {
+
+          if (theText.x == 3.563) {
+            var texta = decodeURIComponent(theText.R[0].T);
+
+            if (texta.includes("@") && texta.includes(".")) {
+              email = texta;
+              theObj.email = email;
+
+              theTournamentArray.push(theObj);
+
+              theTournamentName = "";
+              number = "";
+              theSchoolName = "";
+              teamName = "";
+              GPA = "";
+              SAT = "";
+              ACT = "";
+              cell = "";
+              contact = "";
+              DOB = "";
+              email = "";
+            }
+          }
+
+          if (theText.x == 3.563) {
             var textb = decodeURIComponent(theText.R[0].T);
-  
-            if(!validator.isEmail(textb)) {
+
+            if (!validator.isEmail(textb)) {
               teamName = textb;
             }
-              
-            
           }
-  
-          if(theText.x == 23.875) {
+
+          if (theText.x == 23.875) {
             var text2 = decodeURIComponent(theText.R[0].T);
-            if(text2 != "GPA") {
+            if (text2 != "GPA") {
               theSchoolName = decodeURIComponent(theText.R[0].T);
             }
-  
-  
           }
-  
-          if(theText.x == 25.125) {
+
+          if (theText.x == 25.125) {
             var text = parseInt(decodeURIComponent(theText.R[0].T));
-            if(text != NaN) {
+            if (text != NaN) {
               GPA = decodeURIComponent(theText.R[0].T);
             }
           }
-  
-          if(theText.x == 27.625) {
+
+          if (theText.x == 27.625) {
             SAT = decodeURIComponent(theText.R[0].T);
           }
-  
-          if(theText.x == 30.125) {
+
+          if (theText.x == 30.125) {
             ACT = decodeURIComponent(theText.R[0].T);
           }
-  
-          if(theText.x == 13.563) {
+
+          if (theText.x == 13.563) {
             cell = decodeURIComponent(theText.R[0].T);
           }
-  
-          if(theText.x == 19.5) {
+
+          if (theText.x == 19.5) {
             contact = decodeURIComponent(theText.R[0].T);
           }
-  
-          if(theText.x == 32.313) {
-            DOB = decodeURIComponent(theText.R[0].T);
 
-            theObj.DOB = DOB;
-            theTournamentArray.push(theObj);
-            
-                        theTournamentName = "";
-                        number = "";
-                        theSchoolName = "";
-                        teamName = "";
-                        GPA = "";
-                        SAT = "";
-                        ACT = "";
-                        cell = "";
-                        contact = "";
-                        DOB = "";
-                        email = "";
+          if (theText.x == 32.313) {
+            DOB = decodeURIComponent(theText.R[0].T);
           }
-  
-  
-  
+
           theObj = {
             theTournamentName,
             number,
@@ -346,65 +134,340 @@ router.get("/show-the-json-two", (req, res) => {
             SAT,
             ACT,
             DOB
-  
-  
-          }
-          
-  
+          };
         });
-  
+
         tournament.push(theTournamentArray);
-  
-  
-  
       }
-  
-  
-    })
-  
-  
-  
-  
+    });
+
     //writeToExcel(tournament)
-    res.send(tournament);
-  
-  
+    res.send(pdfData);
   });
-  
-  
+
   pdfParser.loadPDF("./files/2017_T3_Fall_Face_Off.pdf");
-  })
-  
+});
+
+router.get("/show-the-json-two", (req, res) => {
+  let pdfParser = new PDFParser();
+
+  pdfParser.on("pdfParser_dataError", errData =>
+    console.error(errData.parserError)
+  );
+  pdfParser.on("pdfParser_dataReady", pdfData => {
+    //fs.writeFile("./pdf2json/test/F1040EZ.json", JSON.stringify(pdfData));
+    //var theData = JSON.stringify(pdfData);
+
+    //var theJsonData = JSON.parse(theData);
+
+    var tournament = [];
+    pdfData.formImage.Pages.forEach((thePageObject, index) => {
+      var tournamentName;
+      if (index > 0) {
+        var theTournamentName = "";
+        var number = "";
+        var theSchoolName = "";
+        var teamName = "";
+        var GPA = "";
+        var SAT = "";
+        var ACT = "";
+        var cell = "";
+        var contact = "";
+        var DOB = "";
+        var email = "";
+
+        var theObj = {};
+
+        var theTournamentArray = [];
+
+        thePageObject.Texts.forEach(theText => {
+          if (theText.x == 2 && theText.y == 2.038) {
+            if (isNaN(decodeURIComponent(theText.R[0].T))) {
+              theTournamentName = decodeURIComponent(theText.R[0].T);
+            }
+          }
+
+          if (theText.x == 2) {
+            if (!isNaN(decodeURIComponent(theText.R[0].T))) {
+              number = decodeURIComponent(theText.R[0].T);
+            }
+          }
+
+          if (theText.x == 3.563) {
+            var texta = decodeURIComponent(theText.R[0].T);
+
+            if (texta.includes("@") && texta.includes(".")) {
+              email = texta;
+              theObj.email = email;
+            }
+          }
+
+          if (theText.x == 3.563) {
+            var textb = decodeURIComponent(theText.R[0].T);
+
+            if (!validator.isEmail(textb)) {
+              teamName = textb;
+            }
+          }
+
+          if (theText.x == 23.875) {
+            var text2 = decodeURIComponent(theText.R[0].T);
+            if (text2 != "GPA") {
+              theSchoolName = decodeURIComponent(theText.R[0].T);
+            }
+          }
+
+          if (theText.x == 25.125) {
+            var text = parseInt(decodeURIComponent(theText.R[0].T));
+            if (text != NaN) {
+              GPA = decodeURIComponent(theText.R[0].T);
+            }
+          }
+
+          if (theText.x == 27.625) {
+            SAT = decodeURIComponent(theText.R[0].T);
+          }
+
+          if (theText.x == 30.125) {
+            ACT = decodeURIComponent(theText.R[0].T);
+          }
+
+          if (theText.x == 13.563) {
+            cell = decodeURIComponent(theText.R[0].T);
+          }
+
+          if (theText.x == 19.5) {
+            contact = decodeURIComponent(theText.R[0].T);
+          }
+
+          if (theText.x == 32.313) {
+            DOB = decodeURIComponent(theText.R[0].T);
+
+            theObj.DOB = DOB;
+            theTournamentArray.push(theObj);
+
+            theTournamentName = "";
+            number = "";
+            theSchoolName = "";
+            teamName = "";
+            GPA = "";
+            SAT = "";
+            ACT = "";
+            cell = "";
+            contact = "";
+            DOB = "";
+            email = "";
+          }
+
+          theObj = {
+            theTournamentName,
+            number,
+            teamName,
+            email,
+            cell,
+            contact,
+            theSchoolName,
+            GPA,
+            SAT,
+            ACT,
+            DOB
+          };
+        });
+
+        tournament.push(theTournamentArray);
+      }
+    });
+
+    // writeToExcel(tournament)
+    // res.send(tournament);
+    res.send(pdfData);
+  });
+
+  pdfParser.loadPDF("./files/NewMidAtlanticShowcase.pdf");
+});
+
+router.get("/show-the-json-three", (req, res) => {
+  let pdfParser = new PDFParser();
+
+  pdfParser.on("pdfParser_dataError", errData =>
+    console.error(errData.parserError)
+  );
+  pdfParser.on("pdfParser_dataReady", pdfData => {
+    //fs.writeFile("./pdf2json/test/F1040EZ.json", JSON.stringify(pdfData));
+    //var theData = JSON.stringify(pdfData);
+
+    //var theJsonData = JSON.parse(theData);
+
+    var tournament = [];
+    pdfData.formImage.Pages.forEach((thePageObject, index) => {
+      var tournamentName;
+      if (index > 0) {
+        var theTournamentName = "";
+        var number = "";
+        var theSchoolName = "";
+        var teamName = "";
+        var GPA = "";
+        var SAT = "";
+        var ACT = "";
+        var cell = "";
+        var contact = "";
+        var DOB = "";
+        var email = "";
+
+        var theObj = {};
+
+        var theTournamentArray = [];
+
+        thePageObject.Texts.forEach(theText => {
+          if (theText.x == 2 && theText.y == 2.038) {
+            if (isNaN(decodeURIComponent(theText.R[0].T))) {
+              theTournamentName = decodeURIComponent(theText.R[0].T);
+              
+            }
+          }
+
+          // if (theText.x == 2) {
+          //   if (!isNaN(decodeURIComponent(theText.R[0].T))) {
+          //     number = decodeURIComponent(theText.R[0].T);
+          //   }
+          // }
+
+          // if (theText.x == 3.563 && theText.y == "2.869") {
+          //   var texta = decodeURIComponent(theText.R[0].T);
+
+          //   theObj.playerName = texta;
+          // }
+
+          if (theText.x == "3.563") {
+            var texta = decodeURIComponent(theText.R[0].T);
+
+            if (texta) {
+              if (validator.isEmail(texta)) {
+                email = texta;
+                
+                theObj.clubTeamName = theTournamentName;
+                theObj.email = texta;
+                tournament.push(theObj);
+                theObj = {};
+              } else {
+                theObj.playerName = texta;
+              }
+            }
+          }
+
+          // if (theText.x == 3.563) {
+          //   var textb = decodeURIComponent(theText.R[0].T);
+
+          //   if (!validator.isEmail(textb)) {
+          //     teamName = textb;
+          //   }
+          // }
+
+          // if (theText.x == 23.875) {
+          //   var text2 = decodeURIComponent(theText.R[0].T);
+          //   if (text2 != "GPA") {
+          //     theSchoolName = decodeURIComponent(theText.R[0].T);
+          //   }
+          // }
+
+          // if (theText.x == 25.125) {
+          //   var text = parseInt(decodeURIComponent(theText.R[0].T));
+          //   if (text != NaN) {
+          //     GPA = decodeURIComponent(theText.R[0].T);
+          //   }
+          // }
+
+          // if (theText.x == 27.625) {
+          //   SAT = decodeURIComponent(theText.R[0].T);
+          // }
+
+          // if (theText.x == 30.125) {
+          //   ACT = decodeURIComponent(theText.R[0].T);
+          // }
+
+          // if (theText.x == 13.563) {
+          //   cell = decodeURIComponent(theText.R[0].T);
+          // }
+
+          // if (theText.x == 19.5) {
+          //   contact = decodeURIComponent(theText.R[0].T);
+          // }
+
+          // if (theText.x == 32.313) {
+          // if(!isEmpty(theObj)) {
+          // tournament.push(theObj);
+
+          // }
+          // }
+
+          //   theObj = {
+          //     theTournamentName,
+          //     number,
+          //     teamName,
+          //     email,
+          //     cell,
+          //     contact,
+          //     theSchoolName,
+          //     GPA,
+          //     SAT,
+          //     ACT,
+          //     DOB
+          //   };
+        });
+      }
+    });
+
+    // writeToExcel(tournament)
+    // res.send(tournament);
 
 
-var jsonexport = require('jsonexport');
+    var data = [];
+    tournament.forEach(t => {
+      data.push({
+        "CLUB TEAM NAME" : t.clubTeamName,
+        "PLAYER NAME" : t.playerName.split('(')[0],
+        "GRAD YEAR" : t.playerName.split("(")[1].split(")")[0],
+        "EMAIL": t.email
+      })
+    })
+    res.send(data);
+    writeToExcel(data)
+  });
+
+  pdfParser.loadPDF("./files/NewNavyRivalryChallenge.pdf");
+});
+
+var jsonexport = require("jsonexport");
 function writeToExcel(tournamentObject) {
-
   var theFinalObj = [];
 
-  tournamentObject.forEach((pageArray) => {
-    pageArray.forEach((teamObj) => {
-      theFinalObj.push(teamObj);
-    })
-  })
-
+  // tournamentObject.forEach(pageArray => {
+  //   pageArray.forEach(teamObj => {
+  //     theFinalObj.push(teamObj);
+  //   });
+  // });
 
   // var filename = path.join('./output', "output.xlsx");
 
   // var outputFile = jsonXlsx.writeFile(filename, JSON.stringify(theFinalObj));
-  
+
   // console.log(outputFile);
 
-
-  jsonexport(theFinalObj, function(err, csv){
-    if(err) return console.log(err);
+  jsonexport(tournamentObject, function(err, csv) {
+    if (err) return console.log(err);
     //console.log(csv);
 
-    fs.writeFile(__dirname + "/output.csv", csv, (err) => {
+    fs.writeFile(__dirname + "/output.csv", csv, err => {
       console.log(err);
     });
-});
+  });
+}
 
+function isEmpty(obj) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) return false;
+  }
+  return true;
 }
 
 module.exports = router;
